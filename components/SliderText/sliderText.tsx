38e63f80fc6f13277c8fe6fb40553e1a6eed8af5
hoaithanhvo <vohoaithanh52@gmail.com>
2023-03-06 22:15:33 +0700

    lan7

--- components/SliderText/sliderText.tsx
+++ components/SliderText/sliderText.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Typical from "react-typical";
 
 function sliderText() {
-  return <div>sliderText</div>;
+  return (
+    <div>
+      sliderText
+      <div>
+        <Typical
+          steps={["Hello", 1000, "Hello, World!", 500]}
+          loop={Infinity}
+          wrapper="p"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default sliderText;
