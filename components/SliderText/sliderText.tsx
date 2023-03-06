bf91d8ff869dfc12cd89e885d8feb4a05a5e3e2a
hoaithanhvo <vohoaithanh52@gmail.com>
2023-03-06 20:48:03 +0700

    delete_textSlider

--- components/SliderText/sliderText.tsx
+++ components/SliderText/sliderText.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Typical from "react-typical";
-import styles from "./sliderText.module.scss";
-function TypicalExample() {
-  const steps = ["developer ", 1000, "Photograper ", 1000, "Youtuber", 1000];
-  return (
-    <div>
-      <h1>Hi Im VO HOAI THANH</h1>
-      <p>
-        I'm a {""}
-        <Typical loop={Infinity} steps={steps} wrapper="b" />
-      </p>
-    </div>
-  );
-}
+// import Typical from "react-typical";
+// import styles from "./sliderText.module.scss";
+// function TypicalExample() {
+//   const steps = ["developer ", 1000, "Photograper ", 1000, "Youtuber", 1000];
+//   return (
+//     <div>
+//       <h1>Hi Im VO HOAI THANH</h1>
+//       <p>
+//         I'm a {""}
+//         <Typical loop={Infinity} steps={steps} wrapper="b" />
+//       </p>
+//     </div>
+//   );
+// }
 
-export default TypicalExample;
+// export default TypicalExample;
