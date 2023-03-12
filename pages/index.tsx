7d7a89099dbb702e52150432eb1df5522399bf87
hoaithanhvo <vohoaithanh52@gmail.com>
2023-03-12 22:49:33 +0700

    lan18

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,6 @@ export default function Home() {
   return (
     <div className={styles.main}>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
       <div className={styles.pokemon}>
         <div>
           <h1>Pokemon List</h1>
