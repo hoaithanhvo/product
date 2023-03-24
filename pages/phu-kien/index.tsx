0c5a57263003c667da09a8098b460b7a4edac6ce
hoaithanhvo <vohoaithanh52@gmail.com>
2023-03-24 20:31:54 +0700

    update tablet

--- pages/phu-kien/index.tsx
+++ pages/phu-kien/index.tsx
@@ -1,7 +1,77 @@
 import React from "react";
-
+import styles from "./index.module.scss";
 function index() {
-  return <div>index</div>;
+  return (
+    <div className={styles.container}>
+      <p>PHỤ KIỆN NỔI BẬT</p>
+      <div className={styles.main}>
+        <div>
+          <img
+            width={64}
+            height={64}
+            src="https://cdn.tgdd.vn/Category/57/5-Pinsa%CC%A3cdu%CC%9B%CC%A3pho%CC%80ng-120x120.png"
+            alt=""
+          />
+          <p>Sạc dự phòng</p>
+        </div>
+        <div>
+          <img
+            width={64}
+            height={64}
+            src="	https://cdn.tgdd.vn/Category/9518/10-Ca%CC%81psa%CC%A3cchuye%CC%82%CC%89ndo%CC%82%CC%89i-120x120.png
+            "
+            alt=""
+          />
+          <p>Cáp sạc</p>
+        </div>
+        <div>
+          <img
+            width={64}
+            height={64}
+            src="https://cdn.tgdd.vn/Category/54/21-Tainghe-120x120.png"
+            alt=""
+          />
+          <p>Tai nghe </p>
+        </div>
+        <div>
+          <img
+            width={64}
+            height={64}
+            src="https://cdn.tgdd.vn/Category/9458/thongminh-120x120.png"
+            alt=""
+          />
+          <p>Thiệt bị nhà thông minh</p>
+        </div>
+        <div>
+          <img
+            width={64}
+            height={64}
+            src="	https://cdn.tgdd.vn/Category/4728/17-Camerawebcam-120x120.png"
+            alt=""
+          />
+          <p>Camera</p>
+        </div>
+        <div>
+          <img
+            width={64}
+            height={64}
+            src="https://cdn.tgdd.vn/Category/9262/icon-mieng-dan-op-lung-100x100-1.png"
+            alt=""
+          />
+          <p>Miếng dán màn hình</p>
+        </div>
+        <div>
+          <img
+            width={64}
+            height={64}
+            src="https://cdn.tgdd.vn/Category/57/5-Pinsa%CC%A3cdu%CC%9B%CC%A3pho%CC%80ng-120x120.png"
+            alt=""
+          />
+          <p>Sạc dự phòng</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default index;
